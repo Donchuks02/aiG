9770782334754e3f5dd7fae84e1a22a23b2c4938
donchuks <chukwudidavid02@gmail.com>
2026-01-22 10:17:43 +0100

    feature:admin can now add prompt and reference image through the admin panel. Images uploaded goes to cloudinary

--- packages/backend/image_editor/admin.py
+++ packages/backend/image_editor/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.utils.html import format_html
+from .models import PromptTemplate
+
+
+# I use "format_html" to display the actual preview image in the list view so i don't have to click into each one to see what it looks like.
+
+@admin.register(PromptTemplate)
+class PromptTemplateAdmin(admin.ModelAdmin):
+    list_display = ('thumbnail_preview', 'name', 'created_at')
+    search_fields = ('name', 'prompt_text')
+
+    def thumbnail_preview(self, obj):
+        if obj.preview_image:
+            return format_html('<img src="{}" style="width: 50px; height: 50px; object-fit: cover; border-radius: 4px;" />', obj.preview_image.url)
+        return "No Image"
+    
+    thumbnail_preview.short_description = 'Preview'
--- packages/backend/image_editor/models.py
+++ packages/backend/image_editor/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class PromptTemplate(models.Model):
+    name = models.CharField(max_length=100)
     prompt_text = models.TextField()
-    preview_image = models.ImageField(upload_to='templates/')
-    
+    preview_image = CloudinaryField('image', folder="previews/")
+
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
